97a622c0f338fd9a851988fc185a4ccef896d90d
proanja <anjabisako@gmail.com>
2013-07-10 23:14:04 +0000

    test5

--- web.js
+++ web.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 app.get('/', function(request, response) {
   var f = fs.readFileSync('index.html');
   response.send(f.toString('utf-8'));
-  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
