1bde3b5561e60a308e72bbffa37fbcf52e837a30
proanja <anjabisako@gmail.com>
2013-07-10 23:01:08 +0000

    test1

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var f = fs.readFileSync('index.html');
+  var buf = new Buffer(100);
+  buf.write(f);
+  response.send(buf.write(f).toString('utf-8'));
   response.send('Hello World2!');
 });
 
